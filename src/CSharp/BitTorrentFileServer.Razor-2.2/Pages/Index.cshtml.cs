92c17b1f45d050a4d62b09daa053951d0dda77e6
Kovács Ádám <kovacsadam07@outlook.hu>
2019-04-21 09:48:56 +0200

    Razor breadcumb

--- src/CSharp/BitTorrentFileServer.Razor-2.2/Pages/Index.cshtml.cs
+++ src/CSharp/BitTorrentFileServer.Razor-2.2/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,35 @@ namespace BitTorrentFileServer.Razor_2._2.Pages
 {
     public class IndexModel : PageModel
     {
+        public string CurrentPath { get; set; } = "./1/a";
+        public IEnumerable<string> PathParts
+        {
+            get
+            {
+                return CurrentPath.Split('/');
+            }
+        }
+
+        public List<string> FolderNames { get; set; } = new List<string>();
+
         public void OnGet()
         {
+        }
 
+        public void NavigatoToFolderIndex(int index)
+        {
+            CurrentPath = Path.Combine(PathParts.ToList().GetRange(0, index).ToArray());
+        }
+
+        public void OnGetOpen(int index)
+        {
+            ;
+            NavigatoToFolderIndex(index);
+        }
+
+        public void OnPostOpen(int index)
+        {
+            ;
         }
     }
 }
